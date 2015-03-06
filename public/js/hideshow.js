07f5b7f15a31739984b181f3d7372109089f36ee
Partola Mark <partolamark@yandex.ru>
2015-03-06 06:46:00 +0400

    Дизайн: Изменен сайдбар админки

--- public/js/hideshow.js
+++ public/js/hideshow.js
@@ -2,38 +2,9 @@
 
 // this tells jquery to run the function below once the DOM is ready
 $(document).ready(function() {
-
-// choose text for the show/hide link - can contain HTML (e.g. an image)
-var showText='Show';
-var hideText='Hide';
-
-// initialise the visibility check
-var is_visible = false;
-
-// append show/hide links to the element directly preceding the element with a class of "toggle"
-$('.toggle').prev().append(' <a href="#" class="toggleLink">'+hideText+'</a>');
-
-// hide all of the elements with a class of 'toggle'
-$('.toggle').show();
-
-// capture clicks on the toggle links
-$('a.toggleLink').click(function() {
-
-// switch visibility
-is_visible = !is_visible;
-
-// change the link text depending on whether the element is shown or hidden
-if ($(this).text()==showText) {
-$(this).text(hideText);
-$(this).parent().next('.toggle').slideDown('slow');
-}
-else {
-$(this).text(showText);
-$(this).parent().next('.toggle').slideUp('slow');
-}
-
-// return false so any link destination is not followed
-return false;
-
-});
+	var toggleItems = $('.toggleLink');
+	toggleItems.next().slideUp();
+	toggleItems.click(function (event) {
+		$(this).next().slideToggle();
+	});
 });
