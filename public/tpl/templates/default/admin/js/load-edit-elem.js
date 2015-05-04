453bd83cfe542a251c0e676712f9e131541fca28
Partola Mark <partolamark@yandex.ru>
2015-05-04 04:19:18 +0400

    Исправлен баг с подгрузкой страницы изменения элемента

--- public/tpl/templates/default/admin/js/load-edit-elem.js
+++ public/tpl/templates/default/admin/js/load-edit-elem.js
@@ -3,12 +3,15 @@
 
 		$('#callback').off();
 
-		$('.j-elems-container').on('click', function(e){
-			if($(e.target).is($('.j-edit-elem'))){
+		$('.j-elems-container').on('click', '.j-edit-elem', function(e){
+			//if($(e.target).is($('.j-edit-elem'))){
 
-				var title = $('.j-elems-container').find('tr').has('[data-id='+$(e.target).data('id')+']').find('.j-elem-title').text();
-				var content = $('.j-elems-container').find('tr').has('[data-id='+$(e.target).data('id')+']').find('.j-elem-content div').text();
+				//var title = $('.j-elems-container').find('tr').has('[data-id='+$(e.target).data('id')+']').find('.j-elem-title').text();
+				//var content = $('.j-elems-container').find('tr').has('[data-id='+$(e.target).data('id')+']').find('.j-elem-content div').text();
+				var title = $(this).parents('tr').find('.j-elem-title').text();
+				var content = $(this).parents('tr').find('.j-elem-content div').text();
 				var id = $(e.target).data('id');
+
 				$.post( $('.j-dir').html()+'/tpl/templates/default/admin/includes/editElem.php',
 				{
 					'title': title,
@@ -19,7 +22,7 @@
 					$('#callback').html(data);
 				});
 
-			}
+			//}
 
 			e.preventDefault();
 		});
